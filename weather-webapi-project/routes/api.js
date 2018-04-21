5ca8f287741d7cc04280442d93d1fa75ea46281f
chengqian1521 <chengqian1521@qq.com>
2018-04-21 23:42:39 +0800

    1.增加获取城市列表接口
    2.range接口暂未完成

--- weather-webapi-project/routes/api.js
+++ weather-webapi-project/routes/api.js
@@ -1,31 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var redis   = require('redis');
+var redis = require('redis');
 
 const redisIp = '101.201.66.163';
 const redisPort = '6379';
 const redisListName = "weather";
 
-const {promisify} = require('util');
+const { promisify } = require('util');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 
 /**
  * @author chengqian
  * @description 根据日期区间获取数据
- * @param {起始时间时间戳} start 
- * @param {结束时间时间戳} end
+ * @param {起始时间} startTime 
+ * @param {结束时间} endTime
  * @returns 返回的数据集 
  */
-var queryDataByRange = async(start,end)=>{
+var queryDataByRange = async (startTime, endTime) => {
     var redisClient;
 
     try {
-        client = redis.createClient(redisPort,redisIp);
+        client = redis.createClient(redisPort, redisIp);
 
         const lrangeAsync = promisify(client.lrange).bind(client);
         const selectAsync = promisify(client.select).bind(client);
@@ -33,16 +33,13 @@ var queryDataByRange = async(start,end)=>{
         const lindexAsync = promisify(client.lindex).bind(client);
         var res = await selectAsync("1");
 
-        if(res != "OK")   
-        {
-            errCode = 2;
+        if (res != "OK") {
             throw res;
         }
 
         var count = await llenAsync("weather");
 
-        if(isNaN(count) || count <= 0)
-        {
+        if (isNaN(count) || count <= 0) {
             throw "count == 0";
         }
 
@@ -50,139 +47,168 @@ var queryDataByRange = async(start,end)=>{
         var endIndex = count - 1;
         var findIndex = -1;
         //使用插值查找
-        // while(1)
-        // {
-            
-        // } 
+        while (1) {
+
+            if (startIndex < endIndex) {
+                break;
+            }
+
+            var startData = await lindexAsync("weather", startIndex);
+            var endData = await lindexAsync("weather", endIndex);
+
+            if (!startData || !endData)
+                throw "startData==null || endData ==null";
+
+            startData = JSON.parse(startData);
+            endData = JSON.parse(endData);
+
+            //let
+
+        }
 
 
     } catch (error) {
-        
-    }finally{
-        if(client)
+        throw error;
+    } finally {
+        if (client)
             client.quit();
     }
 
 
 
-} 
+}
 
-var queryCityList = async()=>{
+var queryCityList = async (startTime, endTime) => {
     var ret = [];
 
     var client;
     try {
-        client = redis.createClient(redisPort,redisIp);
+        client = redis.createClient(redisPort, redisIp);
         const selectAsync = promisify(client.select).bind(client);
-        const keysAsync =  promisify(client.keys).bind(client);
+        const keysAsync = promisify(client.keys).bind(client);
         const error = await selectAsync("1");
 
-        if(error != "OK")   
-        {
+        if (error != "OK") {
             throw error;
         }
         ret = await keysAsync("*");
     }
-    catch(error)
-    {
-        console.log(error);
+    catch (error) {
         throw error;
     }
-    finally
-    {
-        if(client)
+    finally {
+        if (client)
             client.quit();
         return ret;
     }
 
 }
 
-var sendJson = async(response,code,message,data)=>{
+
+var sendJson = async (response, code, message, data) => {
     response.writeHead(200, { "Content-Type": "application/json" });
     response.end(JSON.stringify({
         code: code,
         message: message,
-        data:data
+        data: data
     }));
 }
 
 
-router.get('/get', async(request, response, next)=>{
-   
+router.get('/get', async (request, response, next) => {
+
     var client;
     var errCode = 0;
     try {
         response.writeHead(200, { "Content-Type": "application/json" });
 
-        let index = parseInt( request.query.index );
-        let num = parseInt( request.query.num );
-    
-        if (isNaN(index) || isNaN(num))
-        {
-           errCode = 1;
-           throw "index or num args error";
+        let index = parseInt(request.query.index);
+        let num = parseInt(request.query.num);
+
+        if (isNaN(index) || isNaN(num)) {
+            errCode = 1;
+            throw "index or num args error";
         }
 
-        client = redis.createClient(redisPort,redisIp);
+        client = redis.createClient(redisPort, redisIp);
 
         const lrangeAsync = promisify(client.lrange).bind(client);
         const selectAsync = promisify(client.select).bind(client);
- 
+
         const error = await selectAsync("1");
 
-        if(error != "OK")   
-        {
+        if (error != "OK") {
             errCode = 2;
             throw error;
         }
-        var queryRes = await lrangeAsync(redisListName,index,index + num -1); 
+        var queryRes = await lrangeAsync(redisListName, index, index + num - 1);
 
-        if(!queryRes)
-        {
+        if (!queryRes) {
             errCode = 3;
-            throw "lrange err:"+queryRes;
+            throw "lrange err:" + queryRes;
         }
 
         var dataArray = [];
 
-        for(var i in queryRes)
-        {
+        for (var i in queryRes) {
             var dataString = queryRes[i];
             dataArray.push(JSON.parse(dataString));
         }
-        
+
         response.end(JSON.stringify({
             code: errCode,
             message: "invoke ok!",
-            data:dataArray
+            data: dataArray
         }));
 
-    }catch(e)
-    {
+    } catch (e) {
         response.end(JSON.stringify({
             code: errCode,
             message: e.toString()
         }));
     }
-    finally
-    {
+    finally {
         // 关闭链接
-        if(client)
+        if (client)
             client.quit();
     }
-    
-   
+
+
 });
 
-router.get('/query',async(request, response, next)=>{
-    
-});
+router.get('/query', async (request, response, next) => {
+    //获取城市
 
-router.get('/cityList',async(request, response, next)=>{
     try {
-        sendJson(response,0,"invoke ok!",await queryCityList());
+
+        let city = request.query.city;
+
+        if (!city) {
+            city = "济南";
+        }
+
+        let startTime = request.query.startTime;
+        let endTime = request.query.endTime;
+
+        if (isNaN(startTime) || startTime < 0)
+            startTime = 0;
+        if (isNaN(endTime))
+            endTime = new Date().getTime();
+
+        sendJson(response, 0, "invoke ok!", await queryDataByRange(startTime, endTime));
     } catch (error) {
-        sendJson(response,1,error,null);
+        sendJson(response, 1, error, null);
+    }
+
+
+
+});
+
+router.get('/cityList', async (request, response, next) => {
+    try {
+        sendJson(response, 0, "invoke ok!", await queryCityList());
+    } catch (error) {
+        sendJson(response, 1, error, null);
     }
 });
 
